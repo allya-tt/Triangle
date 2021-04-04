43528e90acdff0dc26b89a38fb2307855ff5daa3
allya-tt <73088897+allya-tt@users.noreply.github.com>
2021-04-04 22:05:51 +0700

    Update tr.py

--- tr.py
+++ tr.py
@@ -7,8 +7,14 @@ for i in range(0, n):
     for j in range(0, i+1):
         new_list.append(int(math.factorial(i)/(math.factorial(j)*math.factorial(i-j))))
     lists.append(new_list)
+s=len(lists[len(lists)-1])
 for i in range(n):
   print("  " * (n - i), end = " ", sep = " ")
   for j in range(0, i + 1):
-    print('{0:4}'.format(lists[i][j]), end="", sep=" ")
+      if s>12 and s<17:
+          print('{0:5}'.format(lists[i][j]), end="", sep="")
+      elif s>=17:
+          print('{0:7}'.format(lists[i][j]), end="", sep="")
+      else:
+          print('{0:4}'.format(lists[i][j]), end="", sep=" ")
   print()
