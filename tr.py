a59c338696ea667df51446f905a178be93275e20
allya-tt <73088897+allya-tt@users.noreply.github.com>
2021-04-04 16:18:48 +0700

    Update tr.py

--- tr.py
+++ tr.py
@@ -8,7 +8,7 @@ for i in range(0, n):
         new_list.append(int(math.factorial(i)/(math.factorial(j)*math.factorial(i-j))))
     lists.append(new_list)
 for i in range(n):
-  print(" " * (n - i), end = " ", sep = " ")
+  print("  " * (n - i), end = " ", sep = " ")
   for j in range(0, i + 1):
-    print('{0:3}'.format(lists[i][j]), end="", sep=" ")
+    print('{0:3}'.format(lists[i][j]), end=" ", sep=" ")
   print()
